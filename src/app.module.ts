1332e1c5f0cec91527e74ce2d37dcc905ab6b872
Дмитрий Севский <dmitry.sevsky@gmail.com>
2022-07-13 20:52:01 +0300

    refactor: refactor user.contriller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegistrationModule } from './registration/registration.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [RegistrationModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RegistrationModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'changeme',
+      database: 'pet-blog',
+      entities: [],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
