85e984c5604b87728f80985cff66c6e42a4e27e2
Дмитрий Севский <dmitry.sevsky@gmail.com>
2022-07-15 16:40:28 +0300

    Add config system

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn('uuid', { name: 'user_id' })
+  user_id!: number;
+
+  @Column({ name: 'email' })
+  email!: string;
+
+  @Column({ name: 'name' })
+  name!: string;
+
+  @Column({ name: 'isActive' })
+  isActive!: boolean;
+
+  @Column({ name: 'phone', nullable: true })
+  phone?: string;
+
+  @Column({ name: 'created_at', type: 'timestamptz' })
+  created_at!: Date;
+
+  @Column({ name: 'removed_at', type: 'timestamptz', nullable: true })
+  removed_at?: Date;
+}
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   createUser(createUserDto: CreateUserDto) {
     return createUserDto;
   }
@@ -14,5 +22,9 @@ export class UserService {
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user by ${updateUserDto}`;
+  }
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 }
